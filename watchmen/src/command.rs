6dcddf9175c9495672399549ec5b49de2b28a412
ahriknow <ahriknow@ahriknow.com>
2022-09-22 23:55:36 +0800

    ahriknow

--- watchmen/src/command.rs
+++ watchmen/src/command.rs
@@ -3,6 +3,7 @@ pub mod run;
 pub mod stop;
 
 use std::error::Error;
+use std::io::Write;
 use std::process::Stdio;
 
 use crate::command;
@@ -52,6 +53,7 @@ pub async fn exec(args: Vec<String>) -> Result<ExitCode, Box<dyn Error>> {
         "stop" => command::stop::run(&args[2..]).await?,
         "list" => command::list::run(&args[2..]).await?,
         "-d" | "--daemon" => start_daemon(&args[2..]).await?,
+        "-t" | "--terminated" => terminated_daemon(&args[2..]).await?,
         _ => {
             let err: String = format!("watchmen: invalid command '{}'", args[1]);
             return Err(err.into());
@@ -73,6 +75,54 @@ async fn start_daemon(args: &[String]) -> Result<ExitCode, Box<dyn Error>> {
         .arg(args[0].as_str())
         .stdout(Stdio::null())
         .spawn()?;
-    println!("child pid: {}", child.id());
+
+    // 保存 pid
+    let pid = child.id();
+
+    let watchmen_path =
+        std::env::var("WATCHMEN_PATH").unwrap_or_else(|_| "/tmp/watchmen".to_string());
+    let path = std::path::Path::new(&watchmen_path);
+    let path = path.join("watchmen.pid");
+    // 创建文件
+    let mut file = std::fs::File::create(path)?;
+    // 写入 pid
+    file.write_all(pid.to_string().as_bytes())?;
+
+    println!("Start daemon: {}", child.id());
+    Ok(ExitCode::SUCCESS)
+}
+extern "C" {
+    pub fn kill(pid: i32, sig: i32) -> i32;
+}
+async fn terminated_daemon(args: &[String]) -> Result<ExitCode, Box<dyn Error>> {
+    let mut signal = 15;
+    if args.len() == 1 {
+        match args[0].as_str() {
+            "-f" | "--force" | "-9" => {
+                signal = 9;
+            }
+            _ => {
+                let err: String = format!("Invalid signal: '{}'", args[0]);
+                return Err(err.into());
+            }
+        }
+    }
+
+    let watchmen_path =
+        std::env::var("WATCHMEN_PATH").unwrap_or_else(|_| "/tmp/watchmen".to_string());
+    let path = std::path::Path::new(&watchmen_path);
+    let path = path.join("watchmen.pid");
+    let pid = std::fs::read_to_string(path)?;
+    let pid = pid.parse::<i32>()?;
+    let res = unsafe { kill(pid, signal) };
+    if res == 0 {
+        println!("Terminated daemon of pid: {}", pid);
+    } else if res == -1 {
+        println!("Daemon process not exists: {}", pid);
+        return Ok(ExitCode::ERROR);
+    } else {
+        println!("Terminated daemon error with code: {}", res);
+        return Ok(ExitCode::ERROR);
+    }
     Ok(ExitCode::SUCCESS)
 }
